3bc2ef380ed4cfad302e34706c1538e57b7e6707
Laurent-Chiaramonte <chiaramonte.laurent@gmail.com>
2016-06-22 11:44:38 +0200

    cloture

---
+++ HelloWorld/HelloWorld.Tests/MathTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld.Tests;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class MathTests
+    {
+        [TestMethod]
+        public void Factorielle_AvecValeur0_Retourne1()
+        {
+            double resultat = MathHelper.Factorielle(0);
+            Assert.AreEqual(1, resultat);
+        }
+
+        [TestMethod]
+        public void Factorielle_AvecValeur3_Retourne6()
+        {
+            double resultat = MathHelper.Factorielle(3);
+            Assert.AreEqual(6, resultat);
+        }
+
+        [TestMethod]
+        public void Factorielle_AvecValeur15_Retourne1307674368000()
+        {
+            double resultat = MathHelper.Factorielle(15);
+            Assert.AreEqual(1307674368000, resultat);
+        }
+
+
+    }
+}
